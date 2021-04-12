abfe515939141f12a3fafcc34d0306c03ec27a4a
sdutwsl@gmail.com <sdutwsl>
2021-04-12 18:00:58 +0800

    增加爬虫

--- craw/craw.py
+++ craw/craw.py
@@ -1,13 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import requests
 import bs4
+import json
 
-list_page=requests.get("http://share.dmhy.org/",proxies={"http":"http://localhost:8889"})
+list_page=requests.get("http://share.dmhy.org/",proxies={"http":"http://localhost:1080"})
 
 main_bs=bs4.BeautifulSoup(list_page.content,"html.parser")
 main_table=main_bs.find("table",{"class":"tablesorter"})
 
 trs=main_table.findAll("tr")
 
+s=""
+
 for tr in trs:
-    tds=tr.findAll("td")
-    print(tds)
+    td_title=tr.find("td",{"class":"title"})
+    if td_title is not None:
+        maga=tr.find("a",{"class":"download-arrow arrow-magnet"})
+        s+="🆒"+td_title.text.strip()+"🆒"
+        s+="🌧"+maga["href"]+"🌧"
+f=open("tele/bitconn.txt","wb")
+f.write(s.encode('utf-8'))
+f.close()
